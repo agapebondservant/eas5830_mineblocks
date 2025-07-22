97aa8378b9572b81de514ddd20c4f0660f4c6c4a
agapebondservant <agapebondservant@yahoo.com>
2025-07-22 01:05:55 +0000

    updates

--- findBlockNonce.py
+++ findBlockNonce.py
@@ -19,14 +19,14 @@ def mine_block(k, prev_hash, transactions):
         return b'\x00'
 
     # TODO your code to find a nonce here
-    trailing_zeros, lines, num = "0"*k, "".join(transactions), 0
+    trailing_zeros, lines, num = "0"*k, "\n".join(transactions), 0
     hash_binary = ""
     nonce = None
     while not hash_binary.endswith('0'*k):
         nonce = bin(num)
         hash_hex = hashlib.sha256(f"{prev_hash}{lines}{num}".encode('utf-8')).hexdigest()
-        num += 1
         hash_binary = format(int(hash_hex, 16), 'b')
+        num += 1
     
     nonce = nonce.encode('utf-8')
     assert isinstance(nonce, bytes), 'nonce should be of type bytes'
