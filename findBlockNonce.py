8acd30dbde86ae20220a0da254577e66ee2dc725
agapebondservant <agapebondservant@yahoo.com>
2025-07-22 01:24:49 +0000

    updates

--- findBlockNonce.py
+++ findBlockNonce.py
@@ -27,7 +27,7 @@ def mine_block(k, prev_hash, transactions):
         hash_binary = format(int(hash_hex, 16), 'b')
         num += 1
     
-    nonce = f"{hex(num)[2:]}".encode('utf-8')
+    nonce = f"{bin(num)[2:]}".encode('utf-8')
     assert isinstance(nonce, bytes), 'nonce should be of type bytes'
     return nonce
 
